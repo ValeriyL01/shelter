98c209e7942b2dbcff66c30b65e29467552d4cd3
ValeriyL01 <lastovka.valeriy.00@mail.ru>
2023-03-20 21:45:20 +0300

    feat: add page responsive pages.

--- shelter/pages/pets/index.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector(".menu-btn");
-const nav = document.querySelector(".nav");
-const body = document.querySelectorAll(".body")
-btn.addEventListener("click", function () {
-    nav.classList.toggle("nav-list-open");
-   
-});
